93b59bd4556eed21b5bbf05e5bb665cb5cf93ec2
Antônio Érico da Silva Peixoto <a.da.silva.peixoto@avanade.com>
2021-06-17 09:00:45 -0300

    Atualizando-BackEnd

--- backEnd/src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
+++ backEnd/src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -7,6 +7,12 @@
             Id = id;
             RoleName = roleName;
         }
+
+        public EmployeeRole(string roleName)
+        {
+            RoleName = roleName;
+        }
+
         public string Id { get; private set; }
         public string RoleName { get; init; }
 
---
+++ backEnd/src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Services/EmployeeRoleService.cs
@@ -0,0 +1,17 @@
+﻿using Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Interfaces.Services;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Services
+{
+    public class EmployeeRoleService : IEmployeeRoleService
+    {
+        public EmployeeRoleService()
+        {
+
+        }
+
+        public void AddEmployeeRole(Entities.EmployeeRole employeeRole)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
