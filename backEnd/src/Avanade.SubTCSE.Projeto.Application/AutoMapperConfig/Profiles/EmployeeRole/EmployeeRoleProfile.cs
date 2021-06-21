ecaf403b1b390801bfccbf7426a7825202e4927c
Antônio Érico da Silva Peixoto <a.da.silva.peixoto@avanade.com>
2021-06-21 09:14:44 -0300

    Atualizando backEnd

---
+++ backEnd/src/Avanade.SubTCSE.Projeto.Application/AutoMapperConfig/Profiles/EmployeeRole/EmployeeRoleProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Avanade.SubTCSE.Projeto.Application.AutoMapperConfig.Profiles.EmployeeRole
+{
+    public class EmployeeRoleProfile : Profile
+    {
+        public EmployeeRoleProfile()
+        {
+            CreateMap<Dtos.EmployeeRole.EmployeeRoleDto, Domain.Aggregates.EmployeeRole.Entities.EmployeeRole>()
+                .ForCtorParam("roleName", opt => opt.MapFrom(src => src.cargo));
+
+            CreateMap<Domain.Aggregates.EmployeeRole.Entities.EmployeeRole, Dtos.EmployeeRole.EmployeeRoleDto>()
+                .ForMember("cargo", opt => opt.MapFrom(src => src.RoleName));
+        }
+    }
+}
